8b4d70c80afb7bbbf5f95dd78a3a3b7cc4c0a932
edghyhdz <edgar.hdz@hotmail.com>
2020-08-28 21:10:50 +0200

    Added messages when achieving certain distances

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tabulate import tabulate
 import sys
-from GMAIL.email_fetch import GoogleEmailFetch, GmailException
+import os
+# os.path.append("~/Desktop/Projects/Garmin_tekst/garmin/")
+# from GMAIL.email_fetch import GoogleEmailFetch, GmailExckeption
 from GARMIN.garmin import GarminException, GarminFetcher
 from TWILIO.send_message import SendMessageException, SendMessage
-import os
+from GMAIL.email_fetch import GmailException, GoogleEmailFetch
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -18,12 +20,15 @@ matplotlib.use('TkAgg')
 # Make a real main function, as well as plot funct  [ ]
 # Make main task more ORGANIZED FAST!               [ ]
 
+distance_covered = [1.5, 5, 10, 15, 21, 40]
+
 try:    
     cred_path = '/home/edgar/Desktop/Projects/credentials_gmail.json'
     token_path = '/home/edgar/Desktop/Projects/token.pickle'
     gmail = GoogleEmailFetch(cred_path=cred_path, token_path=token_path)
     messages, max_time = gmail.get_email_content()
-
+    url_garmin = gmail.complete_link
+    
     print(messages[max_time].get('complete_link'))
     url = messages[max_time].get('complete_link')
     session_id = messages[max_time].get('session_id')
@@ -37,7 +42,7 @@ try:
     phone_to_send = os.environ.get("TEST_NUMBER")
     message = SendMessage(acc_sid=acc_sid, auth_token=auth_token, own_number=own_number, phone_list=[phone_to_send])
 
-    fig, ax = plt.subplots(1, 1)
+    fig, ax = plt.subplots(1, 2)
 
     def animate(i):
         """
@@ -45,28 +50,53 @@ try:
         Temporary function
         """
         global test
-         
-        df = test.fetch_data()
+        global distance_covered
 
-        if len(df) > 100: 
-            temp_df = df.tail(300)
-        else:
-            temp_df = df
-        
-        heart_rate = temp_df['hb'].iloc[-1]
-        print(heart_rate)
-        if heart_rate > 80: 
-            message.send_messages("Your heart rate is: {}. Nice!!".format(heart_rate))
-            # Stop it from sending more
-            message.message_counter = 10
+        try: 
+            df = test.fetch_data()
 
-        ax.clear()
-        ax.plot(temp_df['timestamp'], temp_df['hb'], color='indianred', linewidth=2)  
-        ax.tick_params(rotation=90, axis='x')
-        ticks = [datetime.strftime(datetime.fromtimestamp(x), '%H:%M:%S') for x in ax.get_xticks()]
-        ax.set_xticklabels(ticks)
-        ax.grid()
-        
+            if len(df) > 100: 
+                temp_df = df.tail(300)
+            else:
+                temp_df = df
+            
+            heart_rate = temp_df['hb'].iloc[-1]
+            current_distance = temp_df['distance'].iloc[-1]
+            
+            idx_pop = None
+            for dis in distance_covered:
+                if dis < current_distance:
+                    idx_pop = distance_covered.index(dis)
+                    distance_covered.pop(idx_pop)
+                    print("Popping out: {}".format(dis))
+                    print("Distances to go: {}".format(distance_covered))
+                    message.send_messages(
+                        "Edgar has covered {} km already! Check his race under: {}".format(
+                            current_distance, url_garmin))
+                    break
+                
+             # if heart_rate > 80: 
+                # message.send_messages("Your heart rate is: {}. Nice!!".format(heart_rate))
+                # Stop it from sending more
+                # message.message_counter = 10
+
+            ax[0].clear()
+            ax[1].clear()
+            ax[0].plot(temp_df['timestamp'], temp_df['hb'], color='indianred', linewidth=2)  
+            ax[0].tick_params(rotation=90, axis='x')
+            ticks = [datetime.strftime(datetime.fromtimestamp(x), '%H:%M:%S') for x in ax[0].get_xticks()]
+            ax[0].set_xticklabels(ticks)
+            ax[0].grid()
+
+            ax[1].plot(temp_df['timestamp'], temp_df['distance'], color='indianred', linewidth=2)  
+            ax[1].tick_params(rotation=90, axis='x')
+            ticks = [datetime.strftime(datetime.fromtimestamp(x), '%H:%M:%S') for x in ax[1].get_xticks()]
+            ax[1].set_xticklabels(ticks)
+            ax[1].grid()
+
+
+        except Exception as e:
+            print(e)
     ani = animation.FuncAnimation(fig, animate, interval=5000)
     plt.show()
     # print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
