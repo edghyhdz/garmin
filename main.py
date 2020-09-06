a9aaafa22b5e41133a30129a8a99054f215ac06d
edghyhdz <edgar.hdz@hotmail.com>
2020-09-06 14:08:55 +0200

    Modified completely main taks

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from tabulate import tabulate
+"""This is the main script to run GarminFetcher class together
+with the SendMessage and GoogleEmailFetch classes.
+
+It fetches from gmail link sent by garmin's LiveTrack, where it then starts the
+GarminFetcher class to continously fetch details of garmin event.
+
+It uses the SendMessage class that is connected to Twilio, to send messages depending
+certain running distances being covered."""
+
 import sys
 import os
 import logging
-from time import sleep
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import matplotlib
 from datetime import datetime
- 
+from time import sleep
+import signal
 # Own libraries
-from GARMIN.garmin import GarminException, GarminFetcher
-from TWILIO.send_message import SendMessageException, SendMessage
-from GMAIL.email_fetch import GmailException, GoogleEmailFetch
-from API.api import User, Events, db
+from GARMIN.garmin import GarminFetcher
+from TWILIO.send_message import SendMessage
+from GMAIL.email_fetch import GoogleEmailFetch
+from API.api import Events, db
 
-matplotlib.use('TkAgg')
-
-# TODO: 
+# TODO:
 # NEED TO RESTART DB -> MODIFIED TABLE EVENTS       [X]
 # Make <event_type> an arg variable for API REQUEST [X]
 # Add a CONFIG CLASS!                               [ ]
@@ -26,85 +29,64 @@ matplotlib.use('TkAgg')
 # Plotly dashboard                                  [ ]
 
 
-if __name__ == "__main__":
-    # CHECK WHY IS IT NOT SAVING
-    import signal
-    import sys
+class MainException(Exception):
+    """
+    Class to handle exceptions for the main script function
+    """
+    error_codes = {
+        '0': "CRED_PATH_GMAIL",
+        '1': "TOKEN_PATH_GMAIL",
+        '2': "ACCOUNT_SID",
+        '3': "AUTH_TOKEN_TWILIO",
+        '4': "OWN_NUMBER",
+        '5': "TEST_NUMBER",
+        '6': "One script is already running"
+    }
 
-    # Check if there are args, else event_type=0
-    if len(sys.argv) > 1: 
-        event_type = sys.argv[1]
-        logging.info("Passed event type: {}".format(event_type))
-    else:
-        event_type = 0
+    def __init__(self, code):
+        self.code = code
+        self.msg = str(code)
 
-    # Distances to reach and update phonelist via twilio
-    distance_covered = [1.5, 5, 10, 15, 21, 40]
+def main(): # pylint: disable-msg=too-many-locals
+    """This is the main script to run GarminFetcher class together
+    with the SendMessage and GoogleEmailFetch classes.
 
-    # Event type 0: Normal, 1: Run, 2: Cycling
-    event_type = 0
+    It fetches from gmail link sent by garmin's LiveTrack, where it then starts the
+    GarminFetcher class to continously fetch details of garmin event.
 
-    # Env variables ##
-    # Gmail
-    cred_path = os.environ.get('CRED_PATH_GMAIL')
-    token_path = os.environ.get('TOKEN_PATH_GMAIL')
+    It uses the SendMessage class that is connected to Twilio, to send messages depending
+    certain running distances being covered.
+    @returns: garmin fetcher
+    """
 
-    # Twilio
-    acc_sid = os.environ.get('ACCOUNT_SID')
-    auth_token = os.environ.get('AUTH_TOKEN_TWILIO')
-    own_number = os.environ.get('OWN_NUMBER')
-    phone_to_send = os.environ.get("TEST_NUMBER")
+    global SESSION_ID
 
-    #DB User ID
-    public_user_id = os.environ.get("DB_USER_ID")
-
-    # Check that all env variables are exported correctly -> none of them should return None
-    env_variables = [cred_path, token_path, acc_sid, auth_token, own_number, phone_to_send]
-
-    class MainException(Exception):
-        """
-        Class to handle exceptions for the main script function
-        """
-        error_codes = {
-            '0': "CRED_PATH_GMAIL",
-            '1': "TOKEN_PATH_GMAIL",
-            '2': "ACCOUNT_SID",
-            '3': "AUTH_TOKEN_TWILIO",
-            '4': "OWN_NUMBER",
-            '5': "TEST_NUMBER",
-            '6': "One script is already running"
-        }
-
-        def __init__(self, code):
-            self.code = code
-            self.msg = str(code)
-
-    try:   
-
-        for k, var in enumerate(env_variables):
+    try:
+        # Check if env variables where initialized correctly
+        for k, var in enumerate(ENV_VARIABLES):
             if not var:
                 logging.error("At least one variable returned None")
                 raise MainException(k)
 
         # Initialize gmail fetch class
-        gmail = GoogleEmailFetch(cred_path=cred_path, token_path=token_path)
+        gmail = GoogleEmailFetch(cred_path=CRED_PATH, token_path=TOKEN_PATH)
         # Get email content and most recent garmin email
         messages, max_time = gmail.get_email_content()
-        # Get email date 
+        # Get email date
         email_date = datetime.fromtimestamp(max_time/1000).date()
-        logging.info("Email date: {}".format(email_date))
+        logging.info("Email date: %s", email_date)
 
         # While email is not that from today's date do not continue
-        # delay = 60 # seconds
-        # while True:
-        #     logging.info("Querying email folder again")
-        #     messages, max_time = gmail.get_email_content()
-        #     email_date = datetime.fromtimestamp(max_time/1000).date()
-        #     logging.info("Email date: {}".format(email_date))
-        #     if email_date == datetime.now().date():
-        #         logging.info("Found email!")
-        #         break
-        #     sleep(delay)
+        delay = 20 # seconds
+        while True:
+            logging.info("Querying email folder again")
+            messages, max_time = gmail.get_email_content()
+            email_date = datetime.fromtimestamp(max_time/1000).date()
+            logging.info("Email date: %s", email_date)
+            if email_date == datetime.now().date():
+                logging.info("Found email!")
+                break
+            sleep(delay)
 
         # Get url from garmin, to be send with an sms to the users on phone_list list
         url_garmin = gmail.complete_link
@@ -114,109 +96,112 @@ if __name__ == "__main__":
         url = messages[max_time].get('complete_link')
 
         # Get session id -> to be used to save csv files with heart bit and distance data
-        session_id = messages[max_time].get('session_id')
+        SESSION_ID = messages[max_time].get('session_id')
 
         # Initilize garminfetcher class
-        garmin_fetcher = GarminFetcher(url=url, session_id=session_id, user_id=public_user_id, event_type=event_type) 
+        garmin_fetcher = GarminFetcher(
+            url=url,
+            session_id=SESSION_ID,
+            user_id=PUBLIC_USER_ID,
+            event_type=EVENT_TYPE
+            )
+
         start_script = garmin_fetcher.check_ongoing_event()
 
         # Start script only if ongoing event is false
         if not start_script:
-            logging.info("Start script?: {}".format(start_script))
+            logging.info("Start script: %s", start_script)
             raise MainException(6)
 
-        # Get data (if any)      
-        df = garmin_fetcher.fetch_data()
+        # Get data (if any)
+        DF_EVENT = garmin_fetcher.fetch_data()
 
         # Initilaize message class
-        message = SendMessage(acc_sid=acc_sid, auth_token=auth_token, own_number=own_number, phone_list=[phone_to_send])
+        message = SendMessage(
+            acc_sid=ACC_SID,
+            auth_token=AUTH_TOKEN,
+            own_number=OWN_NUMBER,
+            phone_list=[PHONE_TO_SEND]
+            )
 
-        # Initialize figure to plot real time
-        nb_plots = 2
-        fig, ax = plt.subplots(nb_plots, 1, figsize=(15, 10), sharex=True)
+        return garmin_fetcher
 
-        # Function to animate realtime plot
-        def animate(i, nb_plots=nb_plots):
-            """
-            Real time plot of the data bein queried
-            Temporary function
-            """
-            global garmin_fetcher
-            global distance_covered
-
-            try: 
-                df = garmin_fetcher.fetch_data()
-
-                if len(df) > 100: 
-                    temp_df = df.tail(300)
-                else:
-                    temp_df = df
-                
-                heart_rate = temp_df['hb'].iloc[-1]
-                current_distance = temp_df['distance'].iloc[-1]
-                
-                idx_pop = None
-                for dis in distance_covered:
-                    if dis < current_distance:
-                        idx_pop = distance_covered.index(dis)
-                        distance_covered.pop(idx_pop)
-                        logging.info("Popping out: {}".format(dis))
-                        logging.info("Distances to go: {}".format(distance_covered))
-                        message.send_messages(
-                            "Edgar has covered {} km already!\n Check his race under: {}".format(
-                                current_distance, url_garmin))
-                        break
-                    
-                # if heart_rate > 80: 
-                    # message.send_messages("Your heart rate is: {}. Nice!!".format(heart_rate))
-                    # Stop it from sending more
-                    # message.message_counter = 10
-
-                for k, concept in zip(range(0, nb_plots), ('hb', 'distance')):
-                    ax[k].clear()
-                    ax[k].plot(temp_df['timestamp'], temp_df[concept], color='indianred', linewidth=2)  
-                    ax[k].tick_params(rotation=90, axis='x')
-                    ticks = [datetime.strftime(datetime.fromtimestamp(x), '%H:%M:%S') for x in ax[k].get_xticks()]
-                    ax[k].set_xticklabels(ticks)
-                    ax[k].grid()
-                
-            except Exception as e:
-                error_line = sys.exc_info()[-1].tb_lineno
-                logging.error("Error: {}. Error line: {}".format(e, error_line))
-
-        # Catch linux signal
-        def signal_term_handler(signal, frame):
-            global session_id
-            print("GOT SIGTERM")
-            logging.info('got SIGTERM')
-            event = Events.query.filter_by(session_id=session_id).first()
-            event.ongoing_event = False
-            db.session.commit()
-            sys.exit(0)
-
-        signal.signal(signal.SIGTERM, signal_term_handler)
-
-        ani = animation.FuncAnimation(fig, animate, interval=5000)
-        plt.show()
-
-    except MainException as e:
+    except MainException as err:
         error_line = sys.exc_info()[-1].tb_lineno
         # Related to started code
-        if e.msg == "6":
+        if err.msg == "6":
             event = Events.query.filter_by(session_id=garmin_fetcher.session_id).first()
             session_id_db = event.session_id
-            logging.error("Error: {}. Event session id: {}. Error line: {}".format(
-                e.error_codes[e.msg], session_id_db, error_line))    
-        else: 
-            logging.error("Variable  <{}> was not initilized. Error line: {}".format(e.error_codes[e.msg], error_line))
+            logging.error("Error: %s. Event session id: %s. Error line: %s", *(
+                err.error_codes[err.msg], session_id_db, error_line))
+        else:
+            error_message = "Variable %s was not initialized. Error line: %s"
+            logging.error(error_message, *(err.error_codes[err.msg], error_line))
 
-    except Exception as e:
+    except Exception as err: # pylint: disable=broad-except
         error_line = sys.exc_info()[-1].tb_lineno
-        logging.error("Error: {}. Error line: {}".format(e, error_line))
+        logging.error("Error: %s. Error line: %s", *(err, error_line))
 
     except KeyboardInterrupt:
-        logging.info("Shuting down app with KeyboardInterrupt") 
+        logging.info("Shuting down app with KeyboardInterrupt")
         event = Events.query.filter_by(session_id=garmin_fetcher.session_id).first()
         event.ongoing_event = False
         event.finished_date = datetime.now()
         db.session.commit()
+
+# Catch linux signal when terminating a proccess with the API
+def signal_term_handler(signal, frame):
+    """
+    Detects whether there was a SIGTERM signal to terminate task
+    """
+    logging.info('got SIGTERM')
+    event = Events.query.filter_by(session_id=SESSION_ID).first()
+    event.ongoing_event = False
+    event.finished_date = datetime.now()
+    db.session.commit()
+    sys.exit(0)
+
+if __name__ == "__main__":
+    # Catches kill signal when using API stop_event call
+    signal.signal(signal.SIGTERM, signal_term_handler)
+
+    # Check if there are args, else EVENT_TYPE=0
+    # Event type 0: Normal, 1: Run, 2: Cycling
+    if len(sys.argv) > 1:
+        EVENT_TYPE = sys.argv[1]
+        logging.info("Passed event type: %s", EVENT_TYPE)
+    else:
+        EVENT_TYPE = 0
+
+    # Distances to reach and update phonelist via twilio
+    DISTANCE_COVERED = [1.5, 5, 10, 15, 21, 40]
+
+    # Env variables ##
+    # Gmail
+    CRED_PATH = os.environ.get('CRED_PATH_GMAIL')
+    TOKEN_PATH = os.environ.get('TOKEN_PATH_GMAIL')
+
+    # Twilio
+    ACC_SID = os.environ.get('ACCOUNT_SID')
+    AUTH_TOKEN = os.environ.get('AUTH_TOKEN_TWILIO')
+    OWN_NUMBER = os.environ.get('OWN_NUMBER')
+    PHONE_TO_SEND = os.environ.get("TEST_NUMBER")
+
+    #DB User ID
+    PUBLIC_USER_ID = os.environ.get("DB_USER_ID")
+
+    # Session ID -> will be overwritten afterwards
+    SESSION_ID = None
+
+    # Garmin fetcher
+    DF_EVENT = None
+
+    # Check that all env variables are exported correctly -> none of them should return None
+    ENV_VARIABLES = [CRED_PATH, TOKEN_PATH, ACC_SID, AUTH_TOKEN, OWN_NUMBER, PHONE_TO_SEND]
+
+    # Run main script
+    GARMIN_FETCHER = main()
+
+    while True:
+        DF_EVENT = GARMIN_FETCHER.fetch_data()
+        sleep(5)
