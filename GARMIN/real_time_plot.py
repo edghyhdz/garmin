b060d4a60c98914ca878289601c347c094995dda
edghyhdz <edgar.hdz@hotmail.com>
2020-09-06 17:19:17 +0200

    Updated animatefunc class, added send_message() method

--- GARMIN/real_time_plot.py
+++ GARMIN/real_time_plot.py
@@ -2,13 +2,9 @@
 
 import sys
 import logging
-import os
 from datetime import datetime
-import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import matplotlib
-
 matplotlib.use('TkAgg')
 
 class AnimateException(Exception):
@@ -28,19 +24,45 @@ class AnimateFunc(object):
     """
     Testing class to animate graph
     """
-
+    # pylint: disable=too-many-arguments
     def __init__(self, ax, distance_covered, url_garmin, nb_plots, garmin_fetcher):
         self.garmin_fetcher: object = garmin_fetcher
         self.distance_covered: list = distance_covered
         self.url_garmin: str = url_garmin
         self.nb_plots: int = nb_plots
-        self.ax: plt.axes = ax
+        self._ax: plt.axes = ax
+
+    def send_message(self, temp_df):
+        """
+        This method pops out from list distances which have been done
+        If a distance has been reached -> sends message to indicated phone number
+        @temp_df: dataframe currently being plotted
+        @returns self.distance_covered list, updated with popped out item
+        """
+        idx_pop = None
+
+        current_distance = temp_df['distance'].iloc[-1]
+
+        for dis in self.distance_covered:
+            if dis < current_distance:
+                idx_pop = self.distance_covered.index(dis)
+                self.distance_covered.pop(idx_pop)
+                logging.info("Popping out: %s", dis)
+                logging.info("Distances to go: %s", self.distance_covered)
+                self.garmin_fetcher.send_messages(
+                    "Edgar has covered %s km already!\n Check his race under: %s" % (
+                        current_distance, self.url_garmin))
+                break
+
+        return None
 
     def animate(self, i):
         """
-        Animate function
+        Animate function used in main script
+        @i: not used, but needed for the animate function from matplotlib
+        @returns: returns plot every x amount of seconds
         """
-        try: 
+        try:
             df_garmin_data = self.garmin_fetcher.fetch_data()
 
             if len(df_garmin_data) > 100:
@@ -48,41 +70,31 @@ class AnimateFunc(object):
             else:
                 temp_df = df_garmin_data
 
-            current_distance = temp_df['distance'].iloc[-1]
-
-            idx_pop = None
-            for dis in self.distance_covered:
-                if dis < current_distance:
-                    idx_pop = self.distance_covered.index(dis)
-                    self.distance_covered.pop(idx_pop)
-                    logging.info("Popping out: %s", dis)
-                    logging.info("Distances to go: %s", self.distance_covered)
-                    self.garmin_fetcher.send_messages(
-                        "Edgar has covered %s km already!\n Check his race under: %s" % (
-                            current_distance, self.url_garmin))
-                    break
+            # See if self.distance_covered needs to be updated and send
+            # message accordingly
+            self.send_message(temp_df=temp_df)
 
             # Plot both heart bit and distance plots
             for k, concept in zip(range(0, self.nb_plots), ('hb', 'distance')):
-                self.ax[k].clear()
-                self.ax[k].plot(
+                self._ax[k].clear()
+                self._ax[k].plot(
                     temp_df['timestamp'],
                     temp_df[concept],
                     color='indianred',
                     linewidth=2
                     )
 
-                self.ax[k].tick_params(rotation=90, axis='x')
+                self._ax[k].tick_params(rotation=90, axis='x')
 
                 # Transform x ticks (timestamp) into datetime object and then to date string
                 ticks = [
                     datetime.strftime(
                         datetime.fromtimestamp(x), '%H:%M:%S'
-                    ) for x in self.ax[k].get_xticks()
+                    ) for x in self._ax[k].get_xticks()
                 ]
 
-                self.ax[k].set_xticklabels(ticks)
-                self.ax[k].grid()
+                self._ax[k].set_xticklabels(ticks)
+                self._ax[k].grid()
 
         except AnimateException as err:
             error_line = sys.exc_info()[-1].tb_lineno
