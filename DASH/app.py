33244d637dbb5b3343f81a35ce314e67bc619e2c
edghyhdz <edgar.hdz@hotmail.com>
2020-09-08 17:27:35 +0200

    First dashboard

--- DASH/app.py
+++ DASH/app.py
@@ -1,12 +1,26 @@
 """Dashboard main app"""
+
+import os
+from datetime import datetime, timedelta
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 import pandas as pd
 
+
 FILE_NAME = '/home/edgar/Desktop/Projects/Garmin_test/garmin/hb_logs/6c8b2c15-540f-4214-b93b-993ab5b660aa_2020_09_03_.csv'
 DF_GARMIN = pd.read_csv(FILE_NAME, index_col=0).tail(300)
+
+GRAPH_INTERVAL = os.environ.get("GRAPH_INTERVAL", 5000)
+
+APP_COLOR = {"graph_bg": "#1f1f1f", "graph_line": "#007ACE"}
+
+# Initiate df_garmin dataframe
+PATH = '/home/edgar/Desktop/Projects/Garmin_test/garmin/hb_logs/c3fc108a-adbc-4807-ba65-1bccc0672cea_2020_09_08_.csv'
+DF_GARMIN = pd.read_csv(PATH, index_col=0)
 
 
 trace_heart_rate = go.Scatter(
@@ -35,45 +49,159 @@ APP.layout = html.Div(
         html.Div(
             [
                 html.H2("Garmin Health Data"),
-                html.Img(src="./assets/stock-icon.png")  
+                html.Img(src="./assets/stock-icon.png"),
             ], 
             className="banner"
         ),
         html.Div(
             [
-                html.H3("Heart Rate"), 
-                dcc.Graph(
-                    id='graph_close',
-                    figure={
-                        "data": [trace_heart_rate],
-                        "layout": {
-                            "title": "Heart Rate"
-                        }
-                    }
-                )
-            
+                html.H2("Test box"),
             ],
-            className="five columns",
+            className='box'
         ),
         html.Div(
-            [   
-                html.H3("Distance"),
-                dcc.Graph(
-                    id='graph_speed',
-                    figure={
-                        "data": [trace_distance],
-                        "layout": {
-                            "title": "Distance"
-                        }
-                    }
-                )
-            ],
-            className="five columns",
+            [
+                html.Div(
+                    [
+                        html.H3("Heart Rate"), 
+                        dcc.Graph(
+                            id="graph_heart_bit",
+                            figure=dict(
+                                layout=dict(
+                                    plot_bgcolor=APP_COLOR["graph_bg"],
+                                    paper_bgcolor=APP_COLOR["graph_bg"],
+                                )
+                            ),
+                        )
+                    ],
+                    className="graph_left",
+                ),
+        html.Div(
+                    [   
+                        html.H3("Distance"),
+                        dcc.Graph(
+                            id='graph_distance',
+                            figure={
+                                "data": [trace_distance],
+                                "layout": {
+                                    "title": "Distance"
+                                }
+                            }
+                        )
+                    ],
+                    className="graph_right",
         )
+            ],
+            className="graph_container"
+        ), 
+        dcc.Interval(
+            id="heart-bit-update",
+            interval=int(GRAPH_INTERVAL),
+            n_intervals=0,
+        ),
     ],
-    className="column"
+    className="row"
 )
+
+@APP.callback(
+    Output("graph_heart_bit", "figure"), [Input("heart-bit-update", "n_intervals")],
+)
+def gen_heart_rate_graph(value):
+    """
+    Generate the heart rate graph.
+
+    :params interval: update the graph based on an interval
+    """
+    global DF_GARMIN
+    DF_GARMIN = pd.read_csv(PATH, index_col=0)
+
+    if len(DF_GARMIN) > 100:
+        DF_GARMIN = DF_GARMIN.tail(300)
+    else:
+        DF_GARMIN = DF_GARMIN
+
+    trace = [dict(
+        type="scatter",
+        y=DF_GARMIN['hb'],
+        x=DF_GARMIN["date"],
+        line={"color": "#f44242"},
+        name="TEST_GRAPH",
+        hovermode="closest",
+        mode="lines",
+        )
+    ]
+
+    layout = dict(
+        plot_bgcolor="#333333",
+        paper_bgcolor="#3d3d3d",
+        font={"color": "#fff"},
+        xaxis={
+            "showline": True,
+            "showline": True,
+            "showgrid": True,
+            "gridcolor": "#D3D3D3",
+            "zeroline": False,
+            "title": "Measurement Timestamp",
+        },
+        yaxis={
+            "showgrid": True,
+            "gridcolor": "#D3D3D3",
+        }
+        
+            )
+
+    return dict(data=trace, layout=layout)
+
+@APP.callback(
+    Output("graph_distance", "figure"), [Input("heart-bit-update", "n_intervals")],
+)
+def gen_distance_graph(value):
+    """
+    Generate the heart rate graph.
+
+    :params interval: update the graph based on an interval
+    """
+    global DF_GARMIN
+    DF_GARMIN = pd.read_csv(PATH, index_col=0)
+
+    if len(DF_GARMIN) > 100:
+        df_distance = DF_GARMIN.tail(300)
+    else:
+        df_distance = DF_GARMIN
+
+    trace = [dict(
+        type="scatter",
+        y=df_distance['distance'],
+        x=DF_GARMIN["date"],
+        line={"color": "#f44242"},
+        name="TEST_GRAPH",
+        hovermode="closest",
+        mode="lines",
+        )
+    ]
+
+    layout = dict(
+        plot_bgcolor="#333333",
+        paper_bgcolor="#3d3d3d",
+        font={"color": "#fff"},
+        xaxis={
+            "showline": True,
+            "showline": True,
+            "showgrid": True,
+            "gridcolor": "#D3D3D3",
+            "zeroline": False,
+            "title": "Measurement Timestamp",
+        },
+        yaxis={
+            "showgrid": True,
+            "gridcolor": "#D3D3D3",
+        }
+        
+            )
+
+    return dict(data=trace, layout=layout)
+
+
 
 if __name__ == "__main__":
     APP.run_server(debug=True)
-
